572c4df21d52370aba811c08148ce77afdecde75
garima <garima01@nagarro.com>
2019-01-31 15:07:03 +0530

    commit changes will be push New file added

---
+++ Learning/src/test/java/Newfile.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author garima01
+ *
+ */
+public class Newfile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		System.out.println("New data");
+		// TODO Auto-generated method stub
+
+	}
+
+}
